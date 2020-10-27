742b1128c6a8107168bcd12b131e1dc784cdabd0
ismayton <ian.mayton@gmail.com>
2020-10-27 14:57:43 -0500

    oauth and has_secure_password flow control

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,22 +16,26 @@ class SessionsController < ApplicationController
     end
     
     def create
-        if auth
+       if !!auth
             @user = User.find_or_create_by(uid: auth['uid']) do |u|
                 u.username = auth['info']['name']
                 u.email = auth['info']['email']
                 u.password = SecureRandom.hex(9)
             end
-        
             session[:user_id] = @user.id 
             redirect_to root_path 
-        elsif @user && @user.authenticate(params[:user][:password])
-            session[:user_id] = @user.id 
-            redirect_to root_path
         else
-            flash.now[:message] = "Invalid Login"
-            render :new 
-        end 
+            @user = User.find_by(username: params[:user][:username])
+
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id 
+                redirect_to root_path
+            else
+                binding.pry
+                flash.now[:message] = "Invalid Login"
+                render :new 
+            end 
+        end
     end 
 
     def delete
